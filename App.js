8417e0f6c5e9c0e6a8ec4edbceaad2af69a51fd2
shinde-ps <107387777+shinde-ps@users.noreply.github.com>
2024-09-02 18:54:42 +0530

    'ep-1'

---
+++ App.js
@@ -0,0 +1,13 @@
+const parent = React.createElement('div', { id: 'parent' }, 
+[React.createElement('div', { id: 'child' }, 
+[React.createElement('h1', { id: 'h1' }, 'I am h1'),
+React.createElement('p', { id: 'p' }, 'I am h1 inside the para')]
+),
+React.createElement('div', { id: 'child2' }, 
+[React.createElement('h1', { id: 'h1' }, 'I am h1'),
+React.createElement('p', { id: 'p' }, 'I am h1 inside the para')]
+)])
+// const heading = React.createElement('h1', { id: 'heading' }, 'Hello React from diff JS!!')
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(parent)
